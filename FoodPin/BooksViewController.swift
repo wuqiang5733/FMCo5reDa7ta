387fc1d728456119b284f916dfda8b8240aa987c
xiaoqianghou <2662651103@qq.com>
2017-03-18 17:32:13 +0800

    Listing 22-22: Updating the table And Deleting a book

--- FoodPin/BooksViewController.swift
+++ FoodPin/BooksViewController.swift
@@ -31,11 +31,13 @@ class BooksViewController: UITableViewController, NSFetchedResultsControllerDele
         }
         return 0
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "booksCell", for: indexPath) as! BooksCell
         updateCell(cell: cell, path: indexPath)
         return cell
     }
+    
     func updateCell(cell: BooksCell, path: IndexPath) {
         let book = fetchedController.object(at: path)
         cell.bookTitle.text = book.title
@@ -49,4 +51,48 @@ class BooksViewController: UITableViewController, NSFetchedResultsControllerDele
             cell.bookAuthor.text = "Not Defined"
         }
     }
+    
+    // Listing 22-22: Updating the table
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .delete:
+            if let path = indexPath {
+                tableView.deleteRows(at: [path], with: .fade)
+            }
+        case .insert:
+            if let path = newIndexPath {
+                tableView.insertRows(at: [path], with: .fade)
+            }
+        case .update:
+            if let path = indexPath {
+                let cell = tableView.cellForRow(at: path) as! BooksCell
+                updateCell(cell: cell, path: path)
+            }
+        default:
+            break
+        }
+    }
+    
+    // Listing 22-20: Deleting a book
+    @IBAction func editBooks(_ sender: UIBarButtonItem) {
+        if tableView.isEditing {
+            tableView.setEditing(false, animated: true)
+        } else {
+            tableView.setEditing(true, animated: true)
+        }
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let book = fetchedController.object(at: indexPath)
+            context.delete(book)
+            do {
+                try context.save()
+//                listOfBooks.remove(at: indexPath.row)
+//                tableView.deleteRows(at: [indexPath], with: .automatic)
+            } catch {
+                print("Error")
+            }
+        }
+    }
+    
 }
